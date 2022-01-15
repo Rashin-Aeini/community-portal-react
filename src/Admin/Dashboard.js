2ae8e45951d090fe21d0bc795028134b400d8bac
Rashin Aeini <aeini.rashin@gmail.com>
2022-01-15 14:38:01 +0330

    Change anchor tag to Link

--- src/Admin/Dashboard.js
+++ src/Admin/Dashboard.js
@@ -1,67 +1,67 @@
 import React from 'react';
 import './Dashboard.css';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 export default function Dashboard(props) {
     return (
         <div>
-            <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-                <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
-                <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+                <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
+                <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="navbar-nav">
-                    <div class="nav-item text-nowrap">
-                        <a class="nav-link px-3" href="#">Sign out</a>
+                <div className="navbar-nav">
+                    <div className="nav-item text-nowrap">
+                        <a className="nav-link px-3" href="#">Sign out</a>
                     </div>
                 </div>
             </header>
 
-            <div class="container-fluid">
-                <div class="row">
-                    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-                        <div class="position-sticky p-3">
-                            <ul class="list-unstyled ps-0">
-                                <li class="mb-1">
-                                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
-                                        <i class="bi bi-pin-fill"></i>
+            <div className="container-fluid">
+                <div className="row">
+                    <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                        <div className="position-sticky p-3">
+                            <ul className="list-unstyled ps-0">
+                                <li className="mb-1">
+                                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="false">
+                                        <i className="bi bi-pin-fill"></i>
                                         Dashboard
                                     </button>
-                                    <div class="collapse" id="home-collapse">
-                                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                    <div className="collapse" id="home-collapse">
+                                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                                             <li>
-                                                <a
-                                                    href="#"
-                                                    class="link-dark rounded">
+                                                <Link
+                                                    to="/admin/index"
+                                                    className="link-dark rounded">
                                                     Overview
-                                                </a>
+                                                </Link>
                                             </li>
                                         </ul>
                                     </div>
                                 </li>
-                                <li class="mb-1">
-                                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#post-collapse" aria-expanded="false">
+                                <li className="mb-1">
+                                    <button className="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#post-collapse" aria-expanded="false">
                                         Post
                                     </button>
-                                    <div class="collapse" id="post-collapse">
-                                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                    <div className="collapse" id="post-collapse">
+                                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                                             <li>
-                                                <a href="#"
-                                                    class="link-dark rounded">
+                                                <Link to="/admin/post"
+                                                    className="link-dark rounded">
                                                     All posts
-                                                </a>
+                                                </Link>
                                             </li>
                                             <li>
-                                                <a href="#"
-                                                    class="link-dark rounded">
+                                                <Link to="/admin/post/create"
+                                                    className="link-dark rounded">
                                                     Add new
-                                                </a>
+                                                </Link>
                                             </li>
                                             <li>
-                                                <a href="#"
-                                                    class="link-dark rounded">
+                                                <Link to="#"
+                                                    className="link-dark rounded">
                                                     Categories
-                                                </a>
+                                                </Link>
                                             </li>
                                         </ul>
                                     </div>
@@ -70,7 +70,7 @@ export default function Dashboard(props) {
                         </div>
                     </nav>
 
-                    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                         <Outlet/>
                     </main>
                 </div>
